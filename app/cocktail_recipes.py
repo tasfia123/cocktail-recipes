290586665e3965bb7769e81a543f5fb5c0bf6bb0
tasfia123 <84350774+tasfia123@users.noreply.github.com>
2021-06-22 20:28:12 -0400

    Update cocktail_recipes.py

--- app/cocktail_recipes.py
+++ app/cocktail_recipes.py
@@ -24,7 +24,7 @@ def liquor_type():
     liquor_data = json.loads(response.text)
 
     random_drink = liquor_data["drinks"][0]
-    random_drink = random.choice[]
+    #random_drink = random.choice[]
 
 
     # for l in (liquor_data["drinks"]):
@@ -57,3 +57,36 @@ liquor_type()
 #         url = i["strDrinkThumb"]
 #         webbrowser.open(url)
 # cocktails()
+
+## Send Email 
+import os
+from dotenv import load_dotenv
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+load_dotenv()
+
+SENDGRID_API_KEY = os.getenv("SENDGRID_API_KEY", default="OOPS, please set env var called 'SENDGRID_API_KEY'")
+SENDER_ADDRESS = os.environ.get("SENDER_ADDRESS", "OOPS, please set env var called 'SENDER_ADDRESS'")
+
+client = SendGridAPIClient(SENDGRID_API_KEY) #> <class 'sendgrid.sendgrid.SendGridAPIClient>
+print("CLIENT:", type(client))
+
+subject = "This is the recipe for your cocktail, enjoy!"
+
+html_content = "Cocktail Recipe"
+print("HTML:", html_content)
+
+message = Mail(from_email=SENDER_ADDRESS, to_emails=SENDER_ADDRESS, subject=subject, html_content=html_content)
+
+try:
+    response = client.send(message)
+
+    print("RESPONSE:", type(response)) #> <class 'python_http_client.client.Response'>
+    print(response.status_code) #> 202 indicates SUCCESS
+    print(response.body)
+    print(response.headers)
+
+except Exception as err:
+    print("OOPS")
+    print(type(err))
+    print(err)
